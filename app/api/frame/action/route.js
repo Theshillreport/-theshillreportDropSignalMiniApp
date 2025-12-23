224ad2f32c0bf56337b639896296663a0f72ce19
Theshillreport <abdoulayenazif289@gmail.com>
2025-12-24 00:23:08 +0100

    route.js aktualisieren

--- app/api/frame/action/route.js
+++ app/api/frame/action/route.js
@@ -1,1 +1,12 @@
-
+export async function POST() {
+  return new Response(
+    `<!DOCTYPE html>
+     <html><head>
+        <meta name="fc:frame" content="vNext" />
+        <meta name="fc:frame:image" content="https://theshillreport-drop-signal-mini-app.vercel.app/IMG_2667.jpeg" />
+     </head><body></body></html>`,
+    {
+      headers: { "Content-Type": "text/html", "Cache-Control": "no-cache" },
+    }
+  );
+}
