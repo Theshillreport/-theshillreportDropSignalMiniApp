e4c4caed39e8494b628183f9cb948647ceb4a850
Theshillreport <abdoulayenazif289@gmail.com>
2025-12-24 01:26:31 +0100

    route.js aktualisieren

--- app/api/frame/route.js
+++ app/api/frame/route.js
@@ -5,15 +5,12 @@ export async function GET() {
   <head>
     <meta charset="utf-8" />
 
-    <!-- OG fallback -->
     <meta property="og:title" content="Drop Signal Mini App" />
     <meta property="og:image" content="https://theshillreport-drop-signal-mini-app.vercel.app/IMG_2667.jpeg" />
 
-    <!-- FRAME aktivieren -->
     <meta name="fc:frame" content="vNext" />
     <meta name="fc:frame:image" content="https://theshillreport-drop-signal-mini-app.vercel.app/IMG_2667.jpeg" />
 
-    <!-- BUTTON -->
     <meta name="fc:frame:button:1" content="Open App" />
     <meta name="fc:frame:button:1:action" content="link" />
     <meta name="fc:frame:button:1:target" content="https://theshillreport-drop-signal-mini-app.vercel.app/" />
