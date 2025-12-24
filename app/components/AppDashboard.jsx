3a741fd77f1527ccc605648b1d1cae18cc98895c
Theshillreport <abdoulayenazif289@gmail.com>
2025-12-24 21:42:34 +0100

    AppDashboard.jsx aktualisieren

--- app/components/AppDashboard.jsx
+++ app/components/AppDashboard.jsx
@@ -1,1 +1,10 @@
+"use client";
 
+export default function AppDashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to DropSignal 🚀</h1>
+      <p>Dashboard coming soon...</p>
+    </div>
+  );
+}
