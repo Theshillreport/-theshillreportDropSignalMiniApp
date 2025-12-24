4b2467cce57243afd206e87e1241dca98e617553
Theshillreport <abdoulayenazif289@gmail.com>
2025-12-24 18:51:37 +0100

    route.js aktualisieren

--- app/api/frame/route.js
+++ app/api/frame/route.js
@@ -1,25 +1,28 @@
 export async function GET() {
-  return new Response(`
+  return new Response(
+    `
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="fc:frame" content="vNext" />
-
     <meta property="og:title" content="Drop Signal" />
     <meta property="og:image" content="https://theshillreport-drop-signal-mini-app.vercel.app/IMG_2667.jpeg" />
 
+    <meta name="fc:frame" content="vNext" />
+
     <meta name="fc:frame:image" content="https://theshillreport-drop-signal-mini-app.vercel.app/IMG_2667.jpeg" />
 
-    <meta name="fc:frame:button:1" content="Open App" />
+    <meta name="fc:frame:button:1" content="Launch App" />
     <meta name="fc:frame:button:1:action" content="link" />
     <meta name="fc:frame:button:1:target" content="https://theshillreport-drop-signal-mini-app.vercel.app/" />
   </head>
+  <body></body>
 </html>
 `,
-  {
-    headers: {
-      "Content-Type": "text/html",
-      "Cache-Control": "no-store",
-    },
-  });
+    {
+      headers: {
+        "Content-Type": "text/html",
+        "Cache-Control": "no-store",
+      },
+    }
+  );
 }
